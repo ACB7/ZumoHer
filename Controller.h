3dec4bd9c22ae49ed7f0c21effb3a6584aeeb2b7
ACB7 <ACBInf1809@gmail.com>
2025-06-26 15:57:17 +0200

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -1,39 +1,26 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include <Zumo32U4.h>
+#include <Arduino.h>
 
 class Controller {
 public:
-    // Constructor
-    Controller();
+    Controller();               // Constructor
 
-    // Vooruit bewegen met huidige snelheid
-    void GoForward();
-
-    // Achteruit bewegen met huidige snelheid
-    void GoBack();
-
-    // Maak een draai naar links of rechts (richting = "left" of "right")
-    void MakeTurn(const char* direction);
-
-    // Versnel tot maxSpeed (optioneel stapgrootte instellen)
-    void Accelerate(int step = 20);
-
-    // Vertraag tot minimaal 0 (optioneel stapgrootte instellen)
-    void Decelerate(int step = 20);
-
-    // Huidige snelheid instellen
-    void SetSpeed(int speed);
-
-    // Huidige snelheid ophalen
-    int GetSpeed() const;
+    void turnLeft();            // Draai naar links
+    void turnRight();           // Draai naar rechts
+    void setSpeed(int speed);   // Stel rijsnelheid in
+    void Stop();                // Stop beide motoren
+    void GoBack();              // Rijd achteruit
+    void TurnLeft();            // Alternatieve schrijfwijze
+    void TurnRight();           // Alternatieve schrijfwijze
 
 private:
-    Zumo32U4Motors motors;
-    int currentSpeed;         // Huidige snelheid van de robot
-    const int maxSpeed = 400; // Maximale motorsnelheid
-    const int minSpeed = 0;   // Minimale snelheid
+    int currentSpeed;           // Huidige snelheid
+    const int maxSpeed = 255;   // Maximale PWM waarde
+    const int minSpeed = 0;     // Minimale PWM waarde
+
+    void applySpeed(int leftSpeed, int rightSpeed); // Helper voor motorregeling
 };
 
 #endif
