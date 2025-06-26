79dbc24f41764a8bc4de338e14bf90b25e62bbdf
ACB7 <ACBInf1809@gmail.com>
2025-06-26 16:02:38 +0200

    Update Controller.cpp

--- Controller.cpp
+++ Controller.cpp
@@ -1,81 +1,46 @@
 #include "Controller.h"
-#include "LineFollower.h"
 
-// Constructor initialiseert motorsnelheid
-Controller::Controller() : currentSpeed(200) {}
+// PWM pinnen voor motors (moet overeenkomen met wiring uit .ino)
+const int LEFT_FORWARD = 9;
+const int LEFT_BACKWARD = 10;
+const int RIGHT_FORWARD = 5;
+const int RIGHT_BACKWARD = 6;
 
-// Vooruit bewegen als lijn recht is
+// Constructor: stel een startsnelheid in
+Controller::Controller() {
+    currentSpeed = 150;  // standaardwaarde
+    pinMode(LEFT_FORWARD, OUTPUT);
+    pinMode(LEFT_BACKWARD, OUTPUT);
+    pinMode(RIGHT_FORWARD, OUTPUT);
+    pinMode(RIGHT_BACKWARD, OUTPUT);
+}
+
+// Stel de snelheid in (binnen grenzen)
+void Controller::setSpeed(int speed) {
+    if (speed > maxSpeed) speed = maxSpeed;
+    if (speed < minSpeed) speed = minSpeed;
+    currentSpeed = speed;
+}
+
+// Laat robot vooruit rijden
 void Controller::GoForward() {
-    LineFollower lf;
-
-    if (lf.FindLine()) {
-        // Zet snelheid op basis van lijnkleur
-        if (lf.isGreenDetected()) {
-            SetSpeed(150);  // langzamer bij groen
-        } else {
-            SetSpeed(300);  // sneller bij zwart
-        }
-
-        // Lees lijnpositie
-        unsigned int sensorValues[5];
-        int position = lf.lineSensors.readLine(sensorValues);
-
-        // Recht vooruit als de lijn gecentreerd is
-        if (position > 1800 && position < 2200) {
-            motors.setSpeeds(currentSpeed, currentSpeed);
-        } else {
-            lf.FollowLine();  // bochten → PID
-        }
-    } else {
-        GoBack();  // lijn kwijt
-    }
+    writeMotors(currentSpeed, 0, currentSpeed, 0);
 }
 
-// Achteruit bewegen tot lijn weer gevonden is
+// Laat robot achteruit rijden
 void Controller::GoBack() {
-    LineFollower lf;
-
-    while (!lf.FindLine()) {
-        motors.setSpeeds(-currentSpeed, -currentSpeed);  // achteruit
-        delay(100);  // korte stapjes
-    }
-
-    // Zodra de lijn teruggevonden is, stop kort en hervat
-    motors.setSpeeds(0, 0);
-    delay(100);
+    writeMotors(0, currentSpeed, 0, currentSpeed);
 }
 
-// Maak een draai naar links of rechts
-void Controller::MakeTurn(const char* direction) {
-    int turnSpeed = currentSpeed;
-
-    if (strcmp(direction, "left") == 0) {
-        motors.setSpeeds(-turnSpeed, turnSpeed); // draai naar links
-    } else if (strcmp(direction, "right") == 0) {
-        motors.setSpeeds(turnSpeed, -turnSpeed); // draai naar rechts
-    } else {
-        motors.setSpeeds(0, 0); // ongeldige input
-    }
-
-    delay(300); // draai even en dan stoppen
-    motors.setSpeeds(0, 0);
-}
-// Huidige snelheid ophalen
-int Controller::GetSpeed() const {
-    return currentSpeed;
+// Stop de robot
+void Controller::Stop() {
+    writeMotors(0, 0, 0, 0);
 }
 
-// Huidige snelheid instellen (geclamped tussen min en max)
-void Controller::SetSpeed(int speed) {
-    currentSpeed = constrain(speed, minSpeed, maxSpeed);
-}
-
-// Verhoog snelheid stapgewijs
-void Controller::Accelerate(int step) {
-    SetSpeed(currentSpeed + step);
-}
-
-// Verlaag snelheid stapgewijs
-void Controller::Decelerate(int step) {
-    SetSpeed(currentSpeed - step);
+// Interne functie: stuur waardes naar de motoren
+void Controller::writeMotors(int leftF, int leftB, int rightF, int rightB) {
+    analogWrite(LEFT_FORWARD, leftF);
+    analogWrite(LEFT_BACKWARD, leftB);
+    analogWrite(RIGHT_FORWARD, rightF);
+    analogWrite(RIGHT_BACKWARD, rightB);
 }
